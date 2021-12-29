16c59393473be5ccd378d8674e8542453d866fd1
Louis Beaumont <louis.beaumont@gmail.com>
2021-12-29 11:27:11 +0000

    😜

--- merge_hacks_into_README.py
+++ merge_hacks_into_README.py
@@ -1,7 +1,7 @@
 # Find [START] in README.md and replace it with MEMORY_STREAM
 import re
 import os
-with open('README.md', 'w') as f:
+with open('README.md', 'r+') as f:
     content = f.read()
     content = re.sub(r'\[START\]', os.environ['MEMORY_STREAM'], content)
     f.write(content)
