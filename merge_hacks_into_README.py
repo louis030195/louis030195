3a24ee03833c4d4ff289e78cf85c43af8179e3f1
Louis Beaumont <louis.beaumont@gmail.com>
2022-12-11 21:07:33 +0100

    e

--- merge_hacks_into_README.py
+++ merge_hacks_into_README.py
@@ -23,17 +23,4 @@ with open('README.md', 'r+') as f:
     except:
         print("🙈fail regex 🙈")
 
-#     {'day': '2022-09-10',
-#  'daily_readiness_score': 75,
-#  'temperature_deviation': -0.1,
-#  'temperature_trend_deviation': 0.17,
-#  'timestamp': '2022-09-10T00:00:00+00:00',
-#  'daily_readiness_activity_balance': 34.0,
-#  'daily_readiness_body_temperature': 100,
-#  'daily_readiness_hrv_balance': 86.0,
-#  'daily_readiness_previous_day_activity': 82,
-#  'daily_readiness_previous_night': 76,
-#  'daily_readiness_recovery_index': 100,
-#  'daily_readiness_resting_heart_rate': 100,
-#  'daily_readiness_sleep_balance': 58.0}
-        
+
