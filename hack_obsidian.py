402d9e5f68cff7c51919b43868701a58745da347
Louis Beaumont <louis.beaumont@gmail.com>
2023-05-27 16:48:26 +0200

    🙈 2023-05-27_16:48:26 🙈

--- hack_obsidian.py
+++ hack_obsidian.py
@@ -5,39 +5,29 @@ import urllib.parse
 TOKEN = sys.argv[1]
 g = Github(TOKEN)
 repo = g.get_repo("louis030195/brain")
-excludeds = [
-    ".obsidian", "css", "Private", ".jpeg", ".png", 
-    ".jpg", "Images", ".gitignore", ".mp4", "Excallidraw",
-    ".vault-stats", "Readwise Syncs",
-    # rnd stuff
-    "Louis"
-    # Human management folder
-    "CRM",
-    "Philosophy/Humans/CRM/talks",
-    "Philosophy/Humans/CRM",
-    # journal
-    "Daily",
-    "Personal growth/Daily"
-]
-includeds = [".md"]
 things = set()
-"""
-mega hack
-"""
+
 skipped = 0
 m = 10
-# TODO: filter out files containing "publish: false"
 for a in repo.get_commits()[0:m+skipped]:
     a: Commit = a
-    lis_list = [f'                    <li><a href="https://brain.louis030195.com/{urllib.parse.quote(f.filename)}">{f.filename}</a></li>' for f in a.files
-        if all([e not in f.filename for e in excludeds]) and
-        # skip deletions
-        f.status != "removed"
-    ]
-    if len(lis_list) == 0:
+# lis_list = [f'                    
+#             <li><a href="https://brain.louis030195.com/{urllib.parse.quote(f.filename)}">{f.filename}</a></li>' for f in a.files
+#         if all([e not in f.filename for e in excludeds]) and
+#         # skip deletions
+#         f.status != "removed"
+#     ]
+    links = []
+    for file in a.files:
+        content = repo.get_contents(file.filename, ref=a.sha).decoded_content.decode()
+        if "publish: true" not in content:
+            continue
+        links.append(f'<li><a href="https://brain.louis030195.com/{urllib.parse.quote(file.filename)}">{file.filename}</a></li>')
+    
+    if len(links) == 0:
         skipped += 1
         continue
-    lis = "\n".join(lis_list)
+    lis = "\n".join(links)
     output = f"""
     <div class="highlight-text" style="margin-left: auto; margin-right: auto; min-width: 280px; max-width: 540px; text-align: left; padding-left: 34px; padding-right: 34px; padding-top: 30px; padding-bottom: 12px; white-space: normal;">
         <span style="background-color: #000000; line-height: 2; padding-bottom: 7px; padding-top: 3px; font-size: 14px; white-space: normal;">
@@ -55,6 +45,22 @@ for a in repo.get_commits()[0:m+skipped]:
     things.add(output)
 things = list(things)
 things_as_str = "\n".join(things)
+if not things_as_str:
+    # say "no brain food today"
+    things_as_str = """
+    <div class="highlight-text" style="margin-left: auto; margin-right: auto; min-width: 280px; max-width: 540px; text-align: left; padding-left: 34px; padding-right: 34px; padding-top: 30px; padding-bottom: 12px; white-space: normal;">
+        <span style="background-color: #000000; line-height: 2; padding-bottom: 7px; padding-top: 3px; font-size: 14px; white-space: normal;">
+            <ul>
+                <li><a href="https://brain.louis030195.com/brain">brain</a></li>
+            </ul>
+        </span>
+        <div style="font-family: Helvetica, Arial, sans-serif;">
+            <div style='font-size: 14px; margin-bottom: 0; margin-top: 10px; font-family: "Raleway", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif; white-space: normal; font-display: swap;'>
+                <p style="margin-bottom: 0; color: #9f8e7d">no brain food today</p>
+            </div>
+        </div>
+    </div>
+    """
 div = f"""
 <div class="some-highlights" style="display: flex;
     margin-left: -50vw;
